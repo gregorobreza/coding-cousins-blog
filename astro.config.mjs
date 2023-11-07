098afde4751e15bcee81450f8f9b5547072330e4
Gregor Obreza <gobreza@gmail.com>
2023-11-07 22:01:06 +0100

    added some integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gregorobreza.github.io',
-    base: '/coding-cousins-blog',
+  site: 'https://gregorobreza.github.io',
+  base: '/coding-cousins-blog',
+  integrations: [preact(), mdx(), tailwind(), sitemap()]
 });

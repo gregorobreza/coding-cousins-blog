e1668582bad590ac14a4fe5a5386e8f9f8aff475
Gregor Obreza <gobreza@gmail.com>
2023-11-08 22:24:09 +0100

    set up tailwind and color switch

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				bkg: 'hsl(var(--color-bkg) / <alpha-value>)',
+				content: 'hsl(var(--color-content) / <alpha-value>)',
+			}
+		},
 	},
 	plugins: [],
 }

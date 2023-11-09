19fd7a83cd1beda4fb29d488aee1d94f11780866
Gregor Obreza <gobreza@gmail.com>
2023-11-09 22:22:54 +0100

    test

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,7 +8,10 @@ export default {
 			colors:{
 				bkg: 'hsl(var(--color-bkg) / <alpha-value>)',
 				content: 'hsl(var(--color-content) / <alpha-value>)',
-			}
+			},
+			fontFamily: {
+				'sans': ['"Titillium Web"', ...defaultTheme.fontFamily.sans],
+			  },
 		},
 	},
 	plugins: [],

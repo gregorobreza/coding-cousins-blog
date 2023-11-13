b7b27bda294d9932a0b21f69aa598e8d3d698064
Gregor Obreza <gobreza@gmail.com>
2023-11-13 21:12:07 +0100

    some changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-	// darkMode: 'class',
 	darkMode: ['class', '[data-mode="dark"]'],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {

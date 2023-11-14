4809c052b9a587dd7d4456db125f8ab0b1caf9f5
Gregor Obreza <gobreza@gmail.com>
2023-11-14 23:23:35 +0100

    some header work

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-	darkMode: ['class', '[data-mode="dark"]'],
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors:{
-				bkg: 'hsl(var(--color-bkg) / <alpha-value>)',
-				content: 'hsl(var(--color-content) / <alpha-value>)',
-			},
-			fontFamily: {
-				'sans': ['"Titillium Web"', ...defaultTheme.fontFamily.sans],
-				'headings': ['"Russo One"', ...defaultTheme.fontFamily.sans]
-			  },
-		},
-	},
-	plugins: [],
-}
+  darkMode: ["class", '[data-mode="dark"]'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bkg: "hsl(var(--color-bkg) / <alpha-value>)",
+        content: "hsl(var(--color-content) / <alpha-value>)",
+        'detail-red': "hsl(var(--color-detail-red) / <alpha-value>)",
+      },
+      fontFamily: {
+        sans: ['"Titillium Web"', ...defaultTheme.fontFamily.sans],
+        headings: ['"Russo One"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
